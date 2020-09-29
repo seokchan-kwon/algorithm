acca0801c6835895baf27e33047c71ce7c6cd6d5
seokchan-kwon <rnjs9603@gmail.com>
2020-09-29 17:18:19 +0900

    quick sort

---
+++ study/sorting/QuickSort.java
@@ -0,0 +1,42 @@
+public class QuickSort {
+    private int[] numbers;
+
+    public QuickSort(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+        while (start <= end) {
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int num = arr[start];
+                arr[start] = arr[end];
+                arr[end] = num;
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private int[] quickSort(int[] arr, int start, int end) {
+        int p = partition(arr, start, end);
+        if (start < p - 1) {
+            quickSort(arr, start, p - 1);
+        }
+        if (p < end) {
+            quickSort(arr, p, end);
+        }
+        return arr;
+    }
+
+    public int[] run() {
+        return quickSort(this.numbers, 0, this.numbers.length - 1);
+    }
+}
